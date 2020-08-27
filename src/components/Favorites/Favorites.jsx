c5a3247a512405af6e16990e1709d5373873805c
Alex Severson <alexsvrsn@gmail.com>
2020-08-27 14:09:40 -0500

    added post, and put routes to server router

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -4,20 +4,9 @@ import Grid from '@material-ui/core/Grid'
 
 class Favorites extends Component {
     render() {
+        
         return (
             <div>
-                  <div>
-                {/* <Grid container>
-                    <Grid item xs={12}>
-                        {this.props.galleryList.map(photo => {
-                            return <GiphyCard
-                                style={{ margin: '3em' }}
-                                key={photo.id}
-                              />
-                        })}
-                    </Grid>
-                </Grid> */}
-            </div>
             </div>
         );
     }
