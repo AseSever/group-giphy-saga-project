24dc7115e1fb5119827a4e8dcf76ca8f3f0379f0
jeremyacoop <63517273+jeremyacoop@users.noreply.github.com>
2020-08-27 10:53:46 -0500

    Created favorites table

--- database.sql
+++ database.sql
@@ -12,3 +12,9 @@ CREATE TABLE "category" (
 -- Default categories. You may change them :)
 INSERT INTO "category" ("name")
 VALUES ('funny'), ('cohort'), ('cartoon'), ('nsfw'), ('meme');
+
+CREATE TABLE "favorites" (
+"id" SERIAL PRIMARY KEY,
+"category_id" INT REFERENCES "category",
+"url" VARCHAR(256) NOT NULL
+);
