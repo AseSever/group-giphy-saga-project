29f512250ad2c1768dba5a4aff79cdbc38f83a75
jeremyacoop <63517273+jeremyacoop@users.noreply.github.com>
2020-08-27 13:16:53 -0500

    added imports

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+import { takeEvery, put } from 'redux-saga/effects';
+
+function* watcherSaga() {
+
+}
+
+const SagaMiddleware = createSagaMiddleware();
+
+const storeInstance = createStore(
+    combineReducers({
+
+    }),
+    applyMiddleware(SagaMiddleware, logger),
+)
 
 ReactDOM.render(<App />, document.getElementById('react-root'));
